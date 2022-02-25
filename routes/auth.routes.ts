54a85bfb615ecae6cbbc14616a3f0002004922df
Leanid Yarmalovich <leanid.yarmalovich@wse-135-71b.icx.local>
2022-02-25 18:13:32 +0300

    fix auth

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,3 +1,6 @@
+import { Response } from 'express';
+import { CustomRequest } from '../middleware/auth.middlewars';
+
 const { Router } = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -6,12 +9,13 @@ const User = require('../models/User.ts');
 
 const router = Router();
 
-router.post('/register', async (req: any, res: any):Promise<void> => {
+router.post('/register', async (req: CustomRequest, res: Response): Promise<void> => {
   try {
     const { email, password, username } = req.body;
     const douplicate = await User.findOne({ email });
     if (douplicate) {
-      return res.status(400).send('User already exist');
+      res.status(400).send('User already exist');
+      return;
     }
     const hashedPassword: String = await bcrypt.hash(password, 12);
     const user = new User({ email, username, password: hashedPassword });
@@ -21,23 +25,25 @@ router.post('/register', async (req: any, res: any):Promise<void> => {
     res.status(500).send('Something is not working');
   }
 });
-router.post('/login', async (req: any, res: any):Promise<void> => {
+router.post('/login', async (req: CustomRequest, res: Response): Promise<void> => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).send('User is not exist');
+      res.status(400).send('User is not exist');
+      return;
     }
     const isMatch: boolean = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).send('Forbidden');
+      res.status(401).send('Forbidden');
+      return;
     }
     const token = jwt.sign(
       { userId: user.id },
       conf.get('tokenSecret'),
       { expiresIn: '1h' },
     );
-    res.send({ token, userId: user.id });
+    res.send({ token });
   } catch (e) {
     res.status(500).send('Something is not working');
   }
