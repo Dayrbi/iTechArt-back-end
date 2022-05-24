b9c033d3f8050c59806b2fee9fe20ce3112a774e
Leanid Yarmalovich <leanid.yarmalovich@wse-135-71b.local>
2022-05-24 20:54:19 +0300

    add requests for get orders

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { createUser, userLogin, authUser } from '../controllers/authController';
+import {
+  createUser, userLogin, authUser, getUser,
+} from '../controllers/authController';
 
 export const authRouter = Router();
 
 authRouter.post('/register', createUser);
 authRouter.post('/login', userLogin);
 authRouter.get('/login', authUser);
+authRouter.get('/getUser', getUser);
